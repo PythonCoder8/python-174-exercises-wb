458fd563340256de24adb9467b84e4891d75675f
PythonCoder8 <72826534+PythonCoder8@users.noreply.github.com>
2021-06-23 10:11:20 -0700

    Update exercise-6.py

--- Intro-to-Programming/exercise-6.py
+++ Intro-to-Programming/exercise-6.py
@@ -1,7 +1,5 @@
-str_cost = input('How much did you pay at the restaurant (without tax or tip)?: $')
-
 try:
-    float_cost = float(str_cost)
+    cost = float(input('How much did you pay at the restaurant (without tax or tip)?: $'))
 except ValueError:
     print('You didn\'t enter a number or decimal for the cost like you should\'ve.')
 
