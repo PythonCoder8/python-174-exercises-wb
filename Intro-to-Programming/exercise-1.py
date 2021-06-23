2d1e612597a7400ee6f17bdb346242c524ec30f0
PythonCoder8 <72826534+PythonCoder8@users.noreply.github.com>
2021-06-23 10:45:25 -0700

    Update exercise-1.py

--- Intro-to-Programming/exercise-1.py
+++ Intro-to-Programming/exercise-1.py
@@ -1,3 +1,9 @@
+'''
+Exercise 1: Mailing Address
+Create a program that displays your name and complete mailing address formatted in
+the manner that you would usually see it on the outside of an envelope. Your program
+does not need to read any input from the user.
+'''
 recipentName = input('Enter your name: ')
 homeAddress = input('Enter your home address: ')
 streetName = input('Enter your street name: ')
@@ -5,4 +11,4 @@ city = input('Enter the name of the city that you live in: ')
 province = input('Enter the name of the province/state that you live in: ')
 postalCode = input('Enter your postal code/zip code: ')
 country = input('Enter the country that you live in: ')
-print(f'\nMailing address:\n\n{recipentName}\n{homeAddress}{ streetName}\n{city} {province} {postalCode}\n{country}')
+print(f'\nMailing address:\n\n{recipentName}\n{homeAddress}{streetName}\n{city} {province} {postalCode}\n{country}')
