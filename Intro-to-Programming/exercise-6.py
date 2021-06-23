c926f59ab08419252e7980ead80c573ba34545d0
PythonCoder8 <72826534+PythonCoder8@users.noreply.github.com>
2021-06-23 10:12:48 -0700

    Update exercise-6.py

--- Intro-to-Programming/exercise-6.py
+++ Intro-to-Programming/exercise-6.py
@@ -6,7 +6,7 @@ tip for the meal. Use your local tax rate when computing the amount of tax owing
 Compute the tip as 18 percent of the meal amount (without the tax). The output from
 your program should include the tax amount, the tip amount, and the grand total for
 the meal including both the tax and the tip. Format the output so that all of the values
-are displayed using two decimal places
+are displayed using two decimal places.
 '''
 
 try:
