c457970b23b0baa284cc89b2c3f2e1570d09a2b8
PythonCoder8 <72826534+PythonCoder8@users.noreply.github.com>
2021-06-23 10:43:09 -0700

    Update exercise-3.py

--- Intro-to-Programming/exercise-3.py
+++ Intro-to-Programming/exercise-3.py
@@ -1,3 +1,11 @@
-length = int(input('Enter the length of the room: '))
-width = int(input('Enter the width of the room: '))
-print(f'The area of the room is {length * width}')
+'''
+Exercise 3: Area of a Room
+Write a program that asks the user to enter the width and length of a room. Once
+the values have been read, your program should compute and display the area of the
+room. The length and the width will be entered as floating point numbers. Include
+units in your prompt and output message; either feet or meters, depending on which
+unit you are more comfortable working with
+'''
+length = int(input('Enter the length of the room in feet: '))
+width = int(input('Enter the width of the room in feet: '))
+print(f'The area of the room is {length * width} square feet.')
